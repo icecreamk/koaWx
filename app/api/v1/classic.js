df4bf5122eb32d9497bde0ad796c12ebd1138fca
llkkxx <550893685@qq.com>
2020-09-06 14:14:37 +0800

    添加全局异常处理中间件

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,10 +1,12 @@
 const Router = require('koa-router')
+const { HttpException } = require('../../../core/http-exception')
 const router = new Router()
 
-router.get('/v1/classic/latest', (ctx, next) => {
+router.post('/v1/:id/classic/latest', (ctx, next) => {
     ctx.body = {
         key: 'classic'
     }
+    throw new HttpException('why', 10001, 400)
 })
 
 module.exports = router
